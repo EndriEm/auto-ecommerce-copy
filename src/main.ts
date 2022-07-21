34c0a08a70b651150b8a2ead2cd663860581de5f
edonajemini <74864689+edonajemini@users.noreply.github.com>
2022-07-21 16:59:28 +0200

    Updated

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ type Car = {
   name: string,
   image: string,
   price: number,
-  stock: number
   year:string
   bodytype: string
   fuel:string
@@ -78,45 +77,6 @@ function renderSearchModal () {
     mainEl.append(wrapperEl)
   }
 
-
-function renderSelectedItemPage(){
-    let mainEl = document.querySelector('main')
-    let selectedItem = state.selectedItem
-
-    let divEl = document.createElement('div')
-    divEl.className = 'selected-item'
-
-    let imgEl = document.createElement('img')
-    imgEl.src = selectedItem.image
-    imgEl.alt = selectedItem.name
-    imgEl.width = 500
-
-  
-    let infoEl = document.createElement('div')
-    infoEl.className = 'selected-item-info'
-    
-    let h3El = document.createElement('h3')
-    h3El.innerText = selectedItem.name
-    h3El.className = 'selected-item-name'
-    let h4El = document.createElement('h4')
-    h4El.innerText = selectedItem?.bodytype
-    h4El.className = 'selected-item-bodytype'
-    let h4El2 = document.createElement('h4')
-    h4El2.innerText = selectedItem?.year
-    let h4El3 = document.createElement('h4')
-    h4El3.innerText = selectedItem?.fuel
-    let phone = document.createElement('p')
-    phone.textContent = 'Phone Number: 048334745849' 
-
-    let priceEl = document.createElement('div')
-    priceEl.className = 'selected-item-price'
-
-
-    infoEl.append( h3El,h4El,h4El2,h4El3,priceEl,phone)
-    divEl.append(imgEl, infoEl)
-    mainEl.append(divEl)
-        
-}
 
 function selectedItem(item: Car) {
     state.selectedItem = item;
@@ -293,7 +253,8 @@ let logoEl = document.querySelector('.the-logo')
     state.searchTerm = ""
     render()
 })
-function postCars(title: string, description: string,) {
+
+function postCars( type: String, name: String, image: String, price: Number, year: String, bodytype:String, fuel: String) {
     let url = 'http://localhost:4999/cars'
     let options = {
       method: 'POST',
@@ -301,8 +262,13 @@ function postCars(title: string, description: string,) {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        title: title,
-        description: description,
+        type: type,
+        name: name,
+        image: image,
+        price: price,
+        year: year,
+        bodytype:bodytype,
+        fuel: fuel
       })
     }
     fetch(url, options)
@@ -316,29 +282,57 @@ function postCars(title: string, description: string,) {
       });
   }
   function renderNewCar() {
-    let sidebarEl = document.querySelector('#app')
+    let sidebarEl = document.querySelector('.sidebar')
     if (sidebarEl === null) return
   
     let carForm = document.createElement('form')
     carForm.addEventListener('submit', function (event) {
       event.preventDefault()
-      postCars(titleInput.value, descriptionInput.value,)
+      postCars(typeInput.value, nameInput.value, imageInput.src, priceInput.value, bodytypeInput.value, yearInput.value, fuelInput.value)
       render()
   
     })
-    let titleInput = document.createElement('input')
-    titleInput.name = 'title'
-    titleInput.placeholder = 'Add car name'
   
-    let descriptionInput = document.createElement('input')
-    descriptionInput.name = 'description'
-    descriptionInput.placeholder = 'Add car description'
+    let typeInput = document.createElement('input')
+    typeInput.type = 'type'
+    typeInput.required = true
+    typeInput.placeholder = 'Add the car type'
+
+    let nameInput = document.createElement('input')
+    nameInput.type = 'type'
+    nameInput.required = true
+    nameInput.placeholder = 'Add the car name'
   
+    let imageInput = document.createElement('img')
+    imageInput.src = 'Image'
+    nameInput.required = true
+    nameInput.placeholder = 'img URL'
+
+    let priceInput = document.createElement('input')
+   priceInput.type = 'Price'
+   priceInput.required = true
+    priceInput.placeholder = 'Add the car price'
+
+     let yearInput = document.createElement('input')
+     yearInput.type = 'Year'
+     yearInput.required = true
+     yearInput.placeholder = 'Add the car year'
+
+     let bodytypeInput = document.createElement('input')
+     bodytypeInput.type = 'bodytype'
+     bodytypeInput.required = true
+     bodytypeInput.placeholder = 'Add the car bodytype'
+
+     let fuelInput = document.createElement('input')
+     fuelInput.type = 'fuel'
+     fuelInput.required = true
+     fuelInput.placeholder = 'Add the car fuel type'
+
     let formCarbtn = document.createElement('button')
     formCarbtn.type = 'submit'
     formCarbtn.textContent = 'submit'
   
-    carForm.append(titleInput, descriptionInput,  formCarbtn)
+    carForm.append( typeInput, nameInput, imageInput, priceInput, yearInput, bodytypeInput,fuelInput, formCarbtn)
     sidebarEl.append(carForm)
   
   }
